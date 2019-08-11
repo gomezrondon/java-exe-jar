59b28b7bb9ca8ac29c622e557996622bc1e46e88
cajgomez <Jgomez@cambridgeassociates.com>
2019-08-10 19:08:53 -0600

    Graal SDK example mixing js with java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
-public class Main {
+import org.graalvm.polyglot.*;
 
+public class Main {
+    //  https://www.graalvm.org/docs/reference-manual/polyglot/
     public static void main(String[] args) {
-        System.out.println("this is a test! ");
+        Context context = Context.newBuilder().allowIO(true).build();
+        Value array = context.eval("js", "[1,2,42,4]");
+        int result = array.getArrayElement(2).asInt(); // we extract the 3rd element from the js array
+        System.out.println(result);
     }
 }
